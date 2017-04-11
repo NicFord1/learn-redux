ab8357890d5b8b78f9109632972e1cf9c4f1994b
Nicholas Ford <Code+NicFord1@Nicks-Net.biz>
2017-04-11 08:49:15 -0400

    Add Support for State Change Actions

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,8 +4,17 @@ console.log('Starting Redux Example');
 
 var reducer = (state = {name: 'Annonymous'}, action) => {
   // state = state || {name: 'Annonymous'};
-  return state;
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {...state, name: action.name};
+    default:
+      return state;
+  }
 };
 var store = redux.createStore(reducer);
 var currentState = store.getState();
 console.log('currentState: ', currentState);
+
+store.dispatch({type: 'CHANGE_NAME', name: 'Nicholas'});
+console.log('currentState: ', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,7 +8,25 @@ var stateDefault = {
   toDos: []
 };
 var reducer = (state = stateDefault, action) => {
-  return state;
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {...state, searchText: action.searchText};
+    case 'CHANGE_SHOW_COMPLETED':
+      return {...state, showCompleted: action.showCompleted};
+    case 'CHANGE_TODOS':
+      return {...state, toDos: action.toDos};
+    default:
+      return state;
+  }
 };
 var store = redux.createStore(reducer);
-console.log('currentState: ', store.getState());
+console.log('currentState after store creation: ', store.getState());
+
+store.dispatch({type: 'CHANGE_SEARCH_TEXT', searchText: 'show'});
+console.log('currentState after change searchText: ', store.getState());
+
+store.dispatch({type: 'CHANGE_SHOW_COMPLETED', showCompleted: true});
+console.log('currentState after change showCompleted: ', store.getState());
+
+store.dispatch({type: 'CHANGE_TODOS', toDos: ['learn git', 'learn react', 'learn redux']});
+console.log('currentState after change toDos: ', store.getState());
